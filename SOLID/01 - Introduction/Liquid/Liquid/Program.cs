d4ee4e3e292220430f1f9b1deabe0d1080877771
Sergio Ferreira <Sergio.Ferreira@vision-box.com>
2018-10-04 14:37:36 +0100

    preping code to give introduction to SOLID lecture

---
+++ SOLID/01 - Introduction/Liquid/Liquid/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liquid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileLines = File.ReadAllLines(".\\FL_insurance_sample.csv");
+
+            for (var i = 1; i < fileLines.Length; i++)
+            {
+                
+            }
+
+
+
+        }
+    }
+}
