bf168278dc145942c608f444bcae0576ec1cfe8a
Sergio Ferreira <Sergio.Ferreira@vision-box.com>
2018-10-04 18:15:42 +0100

    refactoring first step

--- SOLID/01 - Introduction/Liquid/Liquid/Program.cs
+++ SOLID/01 - Introduction/Liquid/Liquid/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 
@@ -11,47 +12,100 @@ namespace Liquid
     // -----
     // Client 1: Ignore Residential type of housing.
     // Client 2: Ignore Commercial type of housing.
-    
+
     // Violations:
     // SRP, OCP, DIP
     public class Program
     {
+        private class InsuranceRecord
+        {
+            public double Value { get; set; }
+            public string TypeOfHouse { get; set; }
+            public string Country { get; set; }
+        }
+
+        class CsvFile
+        {
+            private static InsuranceRecord MapInsuranceRecord(int i, IReadOnlyList<string> fileLines)
+            {
+                var data = fileLines[i].Split(',');
+
+                double.TryParse(data[8], NumberStyles.Any, CultureInfo.InvariantCulture, out var value);
+                return new InsuranceRecord
+                {
+                    Country = data[3],
+                    TypeOfHouse = data[15],
+                    Value = value
+                };
+            }
+
+            public IEnumerable<InsuranceRecord> GetInsuranceRecords()
+            {
+                var fileLines = File.ReadAllLines(".\\FL_insurance_sample.csv");
+
+                var insuranceRecords = new List<InsuranceRecord>();
+
+                for (var i = 1; i < fileLines.Length; i++)
+                {
+                    var record = MapInsuranceRecord(i, fileLines);
+                    insuranceRecords.Add(record);
+                }
+
+                return insuranceRecords;
+            }
+
+            public void StartExport(int client)
+            {
+                _output = new StreamWriter(File.OpenWrite($".\\MyOutput{client}.csv"));
+                _output.WriteLine($"Country,Value");
+            }
+
+            public void StoreTotalValue(double total)
+            {
+                _output.WriteLine("{0},{1}", total.ToString("F2", CultureInfo.InvariantCulture), DateTime.Now.ToString("d", CultureInfo.InvariantCulture));
+                _output.Flush();
+                _output.Close();
+            }
+
+            public void StoreRecord(InsuranceRecord record)
+            {
+                _output.WriteLine($"{record.Country},{record.Value.ToString("F2", CultureInfo.InvariantCulture)}");
+            }
+
+            private static StreamWriter _output;
+        }
+
         public static void Main(string[] args)
         {
-            var client = -1;
-            
-            int.TryParse(args[0], out client);
+            var csvFile = new CsvFile();
 
-            var fileLines = File.ReadAllLines(".\\FL_insurance_sample.csv");
-            var output = new StreamWriter(File.OpenWrite($".\\MyOutput{client}.csv"));
-            output.WriteLine($"Country,Value");
+            int.TryParse(args[0], out var client);
+
             var total = 0.0;
-            for (var i = 1; i < fileLines.Length; i++)
+
+            csvFile.StartExport(client);
+
+            foreach (var record in csvFile.GetInsuranceRecords())
             {
-                if (double.TryParse(fileLines[i].Split(',')[8], NumberStyles.Any, CultureInfo.InvariantCulture, out var value))
+                if (client == 1)
                 {
-                    if (client == 1)
+                    if (record.TypeOfHouse == "Residential")
                     {
-                        if (fileLines[i].Split(',')[15] == "Residential")
-                        {
-                            output.WriteLine($"{fileLines[i].Split(',')[3]},{value.ToString("F2", CultureInfo.InvariantCulture)}");
-                            total += value;
-                        }
+                        csvFile.StoreRecord(record);
+                        total += record.Value;
                     }
-                    if (client == 2)
+                }
+                if (client == 2)
+                {
+                    if (record.TypeOfHouse == "Commercial")
                     {
-                        if (fileLines[i].Split(',')[15] == "Commercial")
-                        {
-                            output.WriteLine($"{fileLines[i].Split(',')[3]},{value.ToString("F2", CultureInfo.InvariantCulture)}");
-                            total += value;
-                        }
+                        csvFile.StoreRecord(record);
+                        total += record.Value;
                     }
                 }
             }
 
-            output.WriteLine("{0},{1}", total.ToString("F2", CultureInfo.InvariantCulture), DateTime.Now.ToString("d", CultureInfo.InvariantCulture));
-            output.Flush();
-            output.Close();
+            csvFile.StoreTotalValue(total);
         }
     }
 }
