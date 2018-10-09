9c58526929198e1d77556c41a4251f0a730fd515
Sergio Ferreira <Sergio.Ferreira@vision-box.com>
2018-10-09 15:22:09 +0100

    moved types to files

---
+++ SOLID/01 - Introduction/Liquid/Liquid/CsvFile.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+
+namespace Liquid
+{
+    internal class CsvFile
+    {
+        private static InsuranceRecord MapInsuranceRecord(int i, IReadOnlyList<string> fileLines)
+        {
+            string[] data = fileLines[i].Split(',');
+
+            double.TryParse(data[8], NumberStyles.Any, CultureInfo.InvariantCulture, out double value);
+            return new InsuranceRecord
+            {
+                Country = data[3],
+                TypeOfHouse = data[15],
+                Value = value
+            };
+        }
+
+        public IEnumerable<InsuranceRecord> GetInsuranceRecords()
+        {
+            string[] fileLines = File.ReadAllLines(".\\FL_insurance_sample.csv");
+
+            List<InsuranceRecord> insuranceRecords = new List<InsuranceRecord>();
+
+            for (int i = 1; i < fileLines.Length; i++)
+            {
+                InsuranceRecord record = MapInsuranceRecord(i, fileLines);
+                insuranceRecords.Add(record);
+            }
+
+            return insuranceRecords;
+        }
+
+        public void StartExport(int client)
+        {
+            _output = new StreamWriter(File.OpenWrite($".\\MyOutput{client}.csv"));
+            _output.WriteLine($"Country,Value");
+        }
+
+        public void StoreTotalValue(double total)
+        {
+            _output.WriteLine("{0},{1}", total.ToString("F2", CultureInfo.InvariantCulture), DateTime.Now.ToString("d", CultureInfo.InvariantCulture));
+            _output.Flush();
+            _output.Close();
+        }
+
+        public void StoreRecord(InsuranceRecord record)
+        {
+            _output.WriteLine($"{record.Country},{record.Value.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+
+        private static StreamWriter _output;
+    }
+}
--- SOLID/01 - Introduction/Liquid/Liquid/Program.cs
+++ SOLID/01 - Introduction/Liquid/Liquid/Program.cs
@@ -1,79 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.IO;
-
-namespace Liquid
+﻿namespace Liquid
 {
     // Read from a csv file
     // Get the total value of insured houses
     // Store the Value by country
     // The last line must have the total and the current date
     // -----
-    // Client 1: Ignore Residential type of housing.
-    // Client 2: Ignore Commercial type of housing.
+    // Client 1: Residential type of housing.
+    // Client 2: Commercial type of housing.
 
-    // Violations:
-    // SRP, OCP, DIP
     public class Program
     {
-        private class InsuranceRecord
-        {
-            public double Value { get; set; }
-            public string TypeOfHouse { get; set; }
-            public string Country { get; set; }
-        }
-
-        class CsvFile
-        {
-            private static InsuranceRecord MapInsuranceRecord(int i, IReadOnlyList<string> fileLines)
-            {
-                var data = fileLines[i].Split(',');
-
-                double.TryParse(data[8], NumberStyles.Any, CultureInfo.InvariantCulture, out var value);
-                return new InsuranceRecord
-                {
-                    Country = data[3],
-                    TypeOfHouse = data[15],
-                    Value = value
-                };
-            }
-
-            public IEnumerable<InsuranceRecord> GetInsuranceRecords()
-            {
-                var fileLines = File.ReadAllLines(".\\FL_insurance_sample.csv");
-
-                var insuranceRecords = new List<InsuranceRecord>();
-
-                for (var i = 1; i < fileLines.Length; i++)
-                {
-                    var record = MapInsuranceRecord(i, fileLines);
-                    insuranceRecords.Add(record);
-                }
-
-                return insuranceRecords;
-            }
-
-            public void StartExport(int client)
-            {
-                _output = new StreamWriter(File.OpenWrite($".\\MyOutput{client}.csv"));
-                _output.WriteLine($"Country,Value");
-            }
-
-            public void StoreTotalValue(double total)
-            {
-                _output.WriteLine("{0},{1}", total.ToString("F2", CultureInfo.InvariantCulture), DateTime.Now.ToString("d", CultureInfo.InvariantCulture));
-                _output.Flush();
-                _output.Close();
-            }
-
-            public void StoreRecord(InsuranceRecord record)
-            {
-                _output.WriteLine($"{record.Country},{record.Value.ToString("F2", CultureInfo.InvariantCulture)}");
-            }
-
-            private static StreamWriter _output;
-        }
 
         public static void Main(string[] args)
         {
