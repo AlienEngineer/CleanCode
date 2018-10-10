c0b86b190ee091e8c83712a4b4aea90775affac5
Sergio Ferreira <Sergio.Ferreira@vision-box.com>
2018-10-10 17:15:28 +0100

    implemented the GroupByCountryReader

---
+++ SOLID/01 - Introduction/Liquid/Liquid/IO/GroupByCountryReader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Liquid.Models;
+
+namespace Liquid.IO
+{
+    internal class GroupByCountryReader : IInsuranceRecordReader
+    {
+        private readonly IInsuranceRecordReader reader;
+
+        public GroupByCountryReader(IInsuranceRecordReader reader)
+        {
+            this.reader = reader;
+        }
+
+        public IEnumerable<InsuranceRecord> GetInsuranceRecords()
+        {
+            return reader.GetInsuranceRecords()
+                .GroupBy(e => e.Country)
+                .Select(groupedByCountry => new InsuranceRecord
+                {
+                    Country = groupedByCountry.Key,
+                    TypeOfHouse = groupedByCountry.First().TypeOfHouse,
+                    Value = groupedByCountry.Sum(e => e.Value)
+                });
+        }
+    }
+}
